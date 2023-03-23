0b89d1547ae7496baa5a269821648e00ed5c6165
ambarayasa <119862512+sidumita-posyandu@users.noreply.github.com>
2023-03-24 04:14:03 +0800

    update validator

--- app/Http/Controllers/API/V1/RoleController.php
+++ app/Http/Controllers/API/V1/RoleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Role;
+use Validator;
 
 class RoleController extends Controller
 {
