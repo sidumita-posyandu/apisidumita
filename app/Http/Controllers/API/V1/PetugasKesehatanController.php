49e36c93c7c22870f15930941bad85e0934f1c07
cahyaw <74125680+cahyawibawa12@users.noreply.github.com>
2023-04-30 16:33:54 +0800

    fix bug

--- app/Http/Controllers/API/V1/PetugasKesehatanController.php
+++ app/Http/Controllers/API/V1/PetugasKesehatanController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\PetugasKesehatan;
+use DB;
 
 class PetugasKesehatanController extends Controller
 {
     public function index()
     {
-        $petugas_kesehatan = auth()->user()->petugas_kesehatan;
+        $petugas_kesehatan = auth()->user();
  
         return response()->json([
             'success' => true,
@@ -54,7 +55,19 @@ class PetugasKesehatanController extends Controller
     public function showMyPetugas(){
 
         // $data = PetugasKesehatan::where("user_id", auth()->user()->id)->first()->id;
-        $petugas_kesehatan = PetugasKesehatan::with(['user'])->where("user_id", auth()->user()->id)->get();
+        // $petugas_kesehatan = PetugasKesehatan::with(['user','dusun'])
+        // ->where("user_id", auth()->user()->id)->get();
+
+        $petugas_kesehatan = DB::table('tb_petugas_kesehatan')
+        ->select('*')
+        // ->join('tb_petugas_kesehatan', 'tb_petugas_kesehatan.dusun_id', '=', 'm_dusun.id')
+        ->join('m_dusun', 'm_dusun.id', '=', 'tb_petugas_kesehatan.dusun_id')
+        ->join('m_desa', 'm_desa.id', '=', 'm_dusun.desa_id')
+        ->join('m_kecamatan', 'm_kecamatan.id', '=', 'm_desa.kecamatan_id')
+        ->join('m_kabupaten', 'm_kabupaten.id', '=', 'm_kecamatan.kabupaten_id')
+        ->join('m_provinsi', 'm_provinsi.id', '=', 'm_kabupaten.provinsi_id')
+        // ->join('users', 'users.id', '=', 'tb_petugas_kesehatan.user_id')
+        ->where("user_id", auth()->user()->id)->get();
 
         return response()->json([
             'status' => true,
