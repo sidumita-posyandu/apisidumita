bd0bae2430cf828823453f93ec86ef4e3e0ca49a
cahyaw <74125680+cahyawibawa12@users.noreply.github.com>
2023-06-18 23:55:56 +0800

    fix bug header email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
 <img src="{{asset('Logo_panjang.png')}}" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
 </a>
 </td>
 </tr>
